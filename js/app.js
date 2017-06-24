2ec182aab4622b3da07f23f81a480b7fb8d5edd8
Alejandra <alejandramelosalazar@gmail.com>
2017-06-24 17:32:09 -0500

    calculadora
    ingresando método de inicialización

---
+++ js/app.js
@@ -0,0 +1,16 @@
+var calculadora = {
+
+  pantalla: document.getElementById("display"),
+  enPantalla : "0",
+  primerNumero : 0,
+  segundoNumero : 0,
+  ultimoNumero: 0,
+  operacion: "",
+  resultado: 0,
+  teclaIgual: false,
+
+  init: (function(){ }),
+
+};
+
+calculadora.init();
